4db5c6a651de0ed1fe91277f45c368a35c046337
Hemanth GS <hemanth.gs@capgemini.com>
2016-11-25 13:06:02 +0530

    cred aws instance

--- cookbooks/webserver/spec/spec_helper.rb
+++ cookbooks/webserver/spec/spec_helper.rb
@@ -4,14 +4,14 @@ describe 'webserver::default' do
   let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe) }
 
   it 'installs a package with an nginx' do
-    expect(chef_run).to install_package('nginx')
+    expect(chef_run).to install_package('httpd')
   end
 
   it 'Starts nginx server' do
-    expect(chef_run).to start_service('nginx')
+    expect(chef_run).to start_service('httpd')
   end
 
   it 'Do not disable nginx server' do
-    expect(chef_run).to_not disable_service('nginx')
+    expect(chef_run).to_not disable_service('httpd')
   end
 end
